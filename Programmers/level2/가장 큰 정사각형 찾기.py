249206d3629723783d6ccd6a629849dc6ad05515
pjok1122 <pjok1122@naver.com>
2020-03-23 00:05:31 +0900

    가장 큰 정사각형 찾기 런타임 오류 수정

--- Programmers/level2/가장 큰 정사각형 찾기.py
+++ Programmers/level2/가장 큰 정사각형 찾기.py
@@ -1,15 +1,19 @@
 def solution(board):
-    dp = [[i for i in row] for row in board]
+    dp = [[num for num in row] for row in board]
     answer = 0
+    if dp[0][0]:
+        answer = 1
 
     for i in range(1, len(dp)):
-        for j in range(1, len(dp)):
+        for j in range(1, len(dp[0])):
+            if dp[i][j] == 0:
+                continue
+            
             dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
             if answer < dp[i][j]:
                 answer = dp[i][j]
-    
 
     return answer**2;
 
 print(solution([[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]))
-
+print(solution([[0, 0, 1, 1], [1, 1, 1, 1]]))
